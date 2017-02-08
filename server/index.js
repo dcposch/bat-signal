412c1c83216b8e9095140b0ed484bbb106795d4c
DC <dcposch@dcpos.ch>
2017-02-08 05:14:19 -0800

    Auto create db dir

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const debug = require('debug')('batsignal')
 const http = require('http')
 const path = require('path')
 const session = require('express-session')
+const mkdirp = require('mkdirp')
 
 const app = require('./app')
 const config = require('../config')
@@ -10,8 +11,10 @@ const socket = require('./socket')
 
 const server = http.createServer()
 
+const dbDir = path.join(config.root, 'db')
+mkdirp.sync(dbDir)
 const SQLiteStore = ConnectSQLite(session)
-const sessionStore = new SQLiteStore({ dir: path.join(config.root, 'db') })
+const sessionStore = new SQLiteStore({ dir: dbDir })
 
 app.init(server, sessionStore)
 socket.init(server, sessionStore)
